f94883fba7ee7be55973597a73c482b13383c591
Björn Harrtell <bjorn@wololo.org>
2021-10-20 16:38:33 +0200

    Invoke and serialize demo function result

---
+++ gsearch-resources/src/main/java/dk/dataforsyningen/gsearch/Demo.java
@@ -0,0 +1,38 @@
+package dk.dataforsyningen.gsearch;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.mapper.reflect.FieldMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Path("demo")
+public class Demo {
+
+    Jdbi jdbi = DatabaseManager.getJdbi();
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<DemoResult> demo() {
+        Logger logger = LoggerFactory.getLogger(Demo.class.getName());
+
+        logger.info("demo called");
+
+        return jdbi.withHandle(handle -> {
+            String sql = "select (api.demo('Farum', NULL, 1, 100)).*";
+            logger.info("Executing SQL " + sql);
+            handle.registerRowMapper(FieldMapper.factory(DemoResult.class));
+            List<DemoResult> results = handle
+                .createQuery(sql)
+                .mapTo(DemoResult.class)
+                .list();
+            return results;
+        });
+    }
+}
