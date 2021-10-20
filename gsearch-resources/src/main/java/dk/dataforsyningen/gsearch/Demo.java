b33e28993e491fc73184e48ba6675973a0c5f6ec
Björn Harrtell <bjorn@wololo.org>
2021-10-20 19:03:54 +0200

    Naive qs parsing

--- gsearch-resources/src/main/java/dk/dataforsyningen/gsearch/Demo.java
+++ gsearch-resources/src/main/java/dk/dataforsyningen/gsearch/Demo.java
@@ -2,9 +2,11 @@ package dk.dataforsyningen.gsearch;
 
 import java.util.List;
 
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.jdbi.v3.core.Jdbi;
@@ -19,13 +21,13 @@ public class Demo {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<DemoResult> demo() {
+    public List<DemoResult> demo(@DefaultValue("Farum") @QueryParam("q") String q) {
         Logger logger = LoggerFactory.getLogger(Demo.class.getName());
 
         logger.info("demo called");
 
         return jdbi.withHandle(handle -> {
-            String sql = "select (api.demo('Farum', NULL, 1, 100)).*";
+            String sql = "select (api.demo('" + q + "', NULL, 1, 100)).*";
             logger.info("Executing SQL " + sql);
             handle.registerRowMapper(FieldMapper.factory(DemoResult.class));
             List<DemoResult> results = handle
