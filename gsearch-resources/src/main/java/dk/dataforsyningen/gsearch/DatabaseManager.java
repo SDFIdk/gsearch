b111246a17eb2e7a1888aede222e500a8a769cf1
Björn Harrtell <bjorn@wololo.org>
2021-10-22 14:19:45 +0200

    Initial dynamic resources implementation

--- gsearch-resources/src/main/java/dk/dataforsyningen/gsearch/DatabaseManager.java
+++ gsearch-resources/src/main/java/dk/dataforsyningen/gsearch/DatabaseManager.java
@@ -1,5 +1,9 @@
 package dk.dataforsyningen.gsearch;
 
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 import javax.ws.rs.ServerErrorException;
 import javax.ws.rs.core.Response;
 
@@ -7,14 +11,23 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.statement.Slf4JSqlLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DatabaseManager {
+    static Logger logger = LoggerFactory.getLogger(DatabaseManager.class);
+
     private static DatabaseManager instance;
 
     private final HikariDataSource hikariDataSource;
     private final Jdbi jdbi;
 
+    private Set<String> types = ConcurrentHashMap.newKeySet();
+
     private DatabaseManager() {
+        logger.debug("constructor called");
+
         // TODO: get data source via JNDI?
         //HikariDataSource hikariDataSource = (HikariDataSource) new InitialContext().lookup("java:comp/env/jdbc/hikariDataSource");
         HikariDataSource hikariDataSource = null;
@@ -43,16 +56,39 @@ public class DatabaseManager {
 
         this.hikariDataSource = hikariDataSource;
         this.jdbi = Jdbi.create(hikariDataSource);
+        if (logger.isDebugEnabled())
+            this.jdbi.setSqlLogger(new Slf4JSqlLogger());
     }
 
     public static DatabaseManager getInstance() {
         return instance;
     }
 
+    public Set<String> getTypes() {
+        return types;
+    }
+
+    private static void determineTypes() {
+        List<String> types = instance.jdbi.withHandle(handle -> {
+            String sql = "select typname from pg_catalog.pg_type t join pg_catalog.pg_namespace pn on (pn.oid = t.typnamespace) join pg_catalog.pg_class pc on (pc.reltype = t.oid) where pn.nspname = 'api' and pc.relkind = 'c'";
+            List<String> typnames = handle
+                .createQuery(sql)
+                .mapTo(String.class)
+                .list();
+            return typnames;
+        });
+        logger.info("Retrieved api types from database: {}", String.join(",", types));
+        // TODO: mutate with diff
+        instance.types.addAll(types);
+    }
+
     public static synchronized void start() throws ServerErrorException {
         if (instance == null) {
+            logger.debug("called first time will create singleton instance");
             try {
                 instance = new DatabaseManager();
+                determineTypes();
+                // TODO: setup notify listener for types
             } catch (Exception ex) {
                 throw new ServerErrorException(Response.Status.INTERNAL_SERVER_ERROR, ex);
             }
@@ -60,6 +96,7 @@ public class DatabaseManager {
     }
 
     public static void shutdown() {
+        logger.debug("shutdown called");
         instance.hikariDataSource.close();
     }
 
--- gsearch-standalone/src/main/java/dk/dataforsyningen/gsearch/Main.java
+++ gsearch-standalone/src/main/java/dk/dataforsyningen/gsearch/Main.java
@@ -2,6 +2,8 @@ package dk.dataforsyningen.gsearch;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.servlet.GrizzlyWebContainerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import java.io.IOException;
@@ -11,9 +13,12 @@ import java.util.Map;
 import java.util.logging.LogManager;
 
 public class Main {
+    static Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static final String BASE_URI = "http://0.0.0.0:8080/";
 
     public static HttpServer startServer() throws IOException {
+        logger.debug("startServer called");
         LogManager.getLogManager().reset();
         SLF4JBridgeHandler.install();
         DatabaseManager.start();
