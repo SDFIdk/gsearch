ea2571619133f4bff3e100f77a36e43bcc3f94be
Björn Harrtell <bjornharrtell@users.noreply.github.com>
2021-10-22 10:40:56 +0200

    Switch to Jackson for JSON serialization (#20)

--- gsearch-servlet/src/main/java/dk/dataforsyningen/gsearch/GSearchApplication.java
+++ gsearch-servlet/src/main/java/dk/dataforsyningen/gsearch/GSearchApplication.java
@@ -2,8 +2,9 @@ package dk.dataforsyningen.gsearch;
 
 import javax.ws.rs.ApplicationPath;
 
-import org.glassfish.jersey.moxy.json.MoxyJsonFeature;
+import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.ServerProperties;
 
 import io.swagger.v3.jaxrs2.integration.resources.AcceptHeaderOpenApiResource;
 import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
@@ -12,8 +13,9 @@ import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 public class GSearchApplication extends ResourceConfig {
   public GSearchApplication() {
     packages("dk.dataforsyningen.gsearch");
-    register(MoxyJsonFeature.class);
+    register(JacksonFeature.class);
     register(OpenApiResource.class);
     register(AcceptHeaderOpenApiResource.class);
+    property(ServerProperties.WADL_FEATURE_DISABLE, true);
   }
 }
