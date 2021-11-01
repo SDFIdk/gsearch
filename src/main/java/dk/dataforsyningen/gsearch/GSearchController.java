1069cb99e3cfcf8d7d613c840b7d6e4342a7f651
Björn Harrtell <bjornharrtell@users.noreply.github.com>
2021-11-01 11:16:21 +0100

    Fix refactoring effort due to missing commits (#40)

--- src/main/java/dk/dataforsyningen/gsearch/GSearchController.java
+++ src/main/java/dk/dataforsyningen/gsearch/GSearchController.java
@@ -1,17 +1,11 @@
 package dk.dataforsyningen.gsearch;
 
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.jdbi.v3.core.Jdbi;
-import org.jdbi.v3.core.mapper.RowMapper;
 import org.jdbi.v3.core.mapper.reflect.FieldMapper;
-import org.jdbi.v3.core.statement.StatementContext;
-import org.locationtech.jts.geom.Geometry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import net.postgis.jdbc.jts.JtsBinaryParser;
 
 @RestController
 public class GSearchController {
@@ -33,45 +25,6 @@ public class GSearchController {
     @Autowired
 	private ResourceTypes resourceTypes;
 
-    JtsBinaryParser binaryParser = new JtsBinaryParser();
-
-    class DataMapper implements RowMapper<Data> {
-        ResultSetMetaData meta;
-        String resource;
-
-        public DataMapper(String resource) {
-            this.resource = resource;
-        }
-
-        private Object mapColumn(int i, ResultSet rs) throws SQLException {
-            if (meta.getColumnTypeName(i).equals("geometry")) {
-                // TODO: find out how to parse binary directly
-                //byte[] bytes = rs.getBytes(i);
-                //Geometry geometry = binaryParser.parse(bytes);
-                String hex = rs.getString(i);
-                Geometry geometry = binaryParser.parse(hex);
-                return geometry;
-            } else {
-                return rs.getString(i);
-            }
-        }
-
-        @Override
-        public Data map(ResultSet rs, StatementContext ctx) throws SQLException {
-            Data data = new Data();
-            data.type = resource;
-            for (int i = 1; i <= meta.getColumnCount(); i++)
-                data.add(meta.getColumnName(i), mapColumn(i, rs));
-            return data;
-        }
-
-        @Override
-        public DataMapper specialize(ResultSet rs, StatementContext ctx) throws SQLException {
-            meta = rs.getMetaData();
-            return this;
-        }
-    }
-
     private List<Data> getData(String search, String resource) {
         return jdbi.withHandle(handle -> {
             String sql = "select (api." + resource + "(:search, NULL, 1, 100)).*";
@@ -79,7 +32,7 @@ public class GSearchController {
             List<Data> data = handle
                 .createQuery(sql)
                 .bind("search", search)
-                .map(new DataMapper(resource))
+                .map(new DataMapper(this, resource))
                 .list();
             return data;
         });
