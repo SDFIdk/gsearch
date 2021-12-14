f0bef5d2207725b93d8b66d120e3f35ad530e174
Björn Harrtell <bjorn@wololo.org>
2021-12-14 21:56:52 +0100

    Remove support for ANY syntax prefer IN

--- src/test/java/dk/dataforsyningen/gsearch/CQLTest.java
+++ src/test/java/dk/dataforsyningen/gsearch/CQLTest.java
@@ -2,17 +2,12 @@ package dk.dataforsyningen.gsearch;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.IOException;
-
 import org.geotools.data.jdbc.FilterToSQL;
 import org.geotools.data.jdbc.FilterToSQLException;
 import org.geotools.data.postgis.PostGISDialect;
-import org.geotools.factory.CommonFactoryFinder;
 import org.geotools.filter.text.cql2.CQLException;
 import org.geotools.filter.text.ecql.ECQL;
 import org.junit.jupiter.api.Test;
-import org.opengis.filter.Filter;
-import org.opengis.filter.FilterFactory;
 
 public class CQLTest {
     static FilterToSQL filterToSQL;
@@ -20,7 +15,7 @@ public class CQLTest {
     static {
         PostGISDialect dialect = new PostGISDialect(null);
         dialect.setFunctionEncodingEnabled(true);
-        filterToSQL = new CustomFilterToSQL(dialect);
+        filterToSQL = dialect.createFilterToSQL();
     }
 
     @Test
@@ -33,13 +28,5 @@ public class CQLTest {
     void ArrayCQLTest() throws CQLException, FilterToSQLException {
         String where = filterToSQL.encodeToString(ECQL.toFilter("a in (1,2)"));
         assertEquals("WHERE a IN (1, 2)", where);
-    }
-
-    @Test
-    void Array2CQLTest() throws CQLException, FilterToSQLException, IOException {
-        FilterFactory filterFactory = CommonFactoryFinder.getFilterFactory();
-        Filter filter = ECQL.toFilter("a=any(1,2)", filterFactory);
-        String where = filterToSQL.encodeToString(filter);
-        assertEquals("WHERE a = ANY (1, 2)", where);
     }
 }
