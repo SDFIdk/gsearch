d2b63c221a3f8a6d09a80113b926845069371ad2
Björn Harrtell <bjornharrtell@users.noreply.github.com>
2021-12-15 18:15:30 +0100

    OpenAPI customization (#55)

--- src/main/java/dk/dataforsyningen/gsearch/Result.java
+++ src/main/java/dk/dataforsyningen/gsearch/Result.java
@@ -2,10 +2,13 @@ package dk.dataforsyningen.gsearch;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 /**
  * Represents a geosearch result response
  */
 public class Result {
+    @Schema(allowableValues = {"OK", "ERROR"})
     public String status;
     public String message;
     public List<Data> data;
