f8cbdbbd174e8cbfb8abf4f172e227056b1b1273
Björn Harrtell <bjorn@wololo.org>
2021-12-15 17:27:10 +0100

    Initial OpenAPI customization

--- src/main/java/dk/dataforsyningen/gsearch/Application.java
+++ src/main/java/dk/dataforsyningen/gsearch/Application.java
@@ -1,13 +1,30 @@
 package dk.dataforsyningen.gsearch;
 
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+import org.jdbi.v3.core.Jdbi;
+import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.media.StringSchema;
+
 @SpringBootApplication
 public class Application {
+
+	@Autowired
+	private Jdbi jdbi;
+
+	@Autowired
+	private ResourceTypes resourceTypes;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -19,5 +36,43 @@ public class Application {
 
 		};
 	}
+
+	Schema<?> getSchema(String resourceType) {
+		Schema<?> schema = new Schema<>();
+		schema.setType("object");
+		schema.properties(getProperties(resourceType));
+		return schema;
+	}
+
+	@Bean
+	public OpenApiCustomiser customerGlobalHeaderOpenApiCustomiser() {
+		return openApi -> {
+			for (String resourceType : resourceTypes.getTypes())
+				openApi.getComponents().addSchemas(resourceType, getSchema(resourceType));
+		};
+	}
+
+    private Schema<?> createSchema(String description) {
+        StringSchema schema = new StringSchema();
+        schema.description(description);
+        return schema;
+    }
+
+    public Map<String, Schema> getProperties(String typname) {
+        return jdbi.withHandle(handle -> {
+            String sql = "select attname, pgd.description\n" +
+            "from pg_catalog.pg_type t\n" +
+            "join pg_catalog.pg_namespace pn on (pn.oid = t.typnamespace)\n" +
+            "join pg_catalog.pg_class pc on (pc.reltype = t.oid)\n" +
+            "join pg_catalog.pg_attribute pa on (t.typrelid = pa.attrelid)\n" +
+            "join pg_catalog.pg_description pgd on (pgd.objoid = pc.oid and pgd.objsubid = pa.attnum)\n" +
+            "where pn.nspname = 'api' and pc.relkind = 'c' and t.typname = :typname;";
+            return handle
+				.createQuery(sql)
+				.bind("typname", typname)
+                .map((rs, ctx) -> new AbstractMap.SimpleEntry<String, Schema<?>>(rs.getString("attname"), createSchema(rs.getString("description"))))
+                .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
+        });
+    }
 
 }
