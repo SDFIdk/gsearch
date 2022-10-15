c4f3bcbc68ddd63427a696172cddc4045cdeef16
Björn Harrtell <bjorn@wololo.org>
2022-10-15 17:51:22 +0200

    Fixes for filter param

---
+++ src/main/java/dk/dataforsyningen/gsearch/rest/CustomPostgisFilterToSQL.java
@@ -0,0 +1,31 @@
+package dk.dataforsyningen.gsearch.rest;
+
+import org.geotools.data.postgis.PostGISDialect;
+import org.geotools.data.postgis.PostgisFilterToSQL;
+import org.opengis.filter.expression.Literal;
+
+/**
+ * Custom GeoTools PostgisFilterToSQL to avoid coercing string literals to numbers
+ * NOTE: This is needed because GeoTools is not aware of the schema and will
+ * will make filtering on text columns possible but not numeric ones
+ */
+public class CustomPostgisFilterToSQL extends PostgisFilterToSQL {
+
+    public CustomPostgisFilterToSQL(PostGISDialect dialect) {
+        super(dialect);
+    }
+
+    public Object evaluateLiteral(Literal expression, Class<?> target) {
+        Object literal = null;
+
+        // if the target was not known, of the conversion failed, try the
+        // type guessing dance literal expression does only for the following
+        // method call
+        if (literal == null) literal = expression.evaluate(null);
+
+        // if that failed as well, grab the value as is
+        if (literal == null) literal = expression.getValue();
+
+        return literal;
+    }
+}
--- src/test/java/dk/dataforsyningen/gsearch/CQLTest.java
+++ src/test/java/dk/dataforsyningen/gsearch/CQLTest.java
@@ -8,6 +8,9 @@ import org.geotools.data.postgis.PostGISDialect;
 import org.geotools.filter.text.cql2.CQLException;
 import org.geotools.filter.text.ecql.ECQL;
 import org.junit.jupiter.api.Test;
+import org.opengis.filter.Filter;
+
+import dk.dataforsyningen.gsearch.rest.CustomPostgisFilterToSQL;
 
 public class CQLTest {
     static FilterToSQL filterToSQL;
@@ -15,18 +18,27 @@ public class CQLTest {
     static {
         PostGISDialect dialect = new PostGISDialect(null);
         dialect.setFunctionEncodingEnabled(true);
-        filterToSQL = dialect.createFilterToSQL();
+        filterToSQL = new CustomPostgisFilterToSQL(dialect);
+        filterToSQL.setInline(true);
     }
 
     @Test
     void BasicCQLTest() throws CQLException, FilterToSQLException {
-        String where = filterToSQL.encodeToString(ECQL.toFilter("a=1"));
-        assertEquals("WHERE a = 1", where);
+        Filter filter = ECQL.toFilter("a = 1");
+        String where = filterToSQL.encodeToString(filter);
+        assertEquals("a = 1", where);
     }
 
     @Test
     void ArrayCQLTest() throws CQLException, FilterToSQLException {
         String where = filterToSQL.encodeToString(ECQL.toFilter("a in (1,2)"));
-        assertEquals("WHERE a IN (1, 2)", where);
+        assertEquals("a IN (1, 2)", where);
+    }
+
+    @Test
+    void TextCQLTest() throws CQLException, FilterToSQLException {
+        Filter filter = ECQL.toFilter("a = '0101'");
+        String where = filterToSQL.encodeToString(filter);
+        assertEquals("a = '0101'", where);
     }
 }
