5e8ad65797123fd037ee78955299cc64c4a0b0f4
Kortforsyningen servicekonto <elvisflesborg@protonmail.com>
2022-11-08 10:24:03 +0100

    max_rows 100 > 1000

--- db/seed/310_api_navngivenvej.sql
+++ db/seed/310_api_navngivenvej.sql
@@ -117,7 +117,7 @@ DECLARE
     stmt text;
 BEGIN
     -- Initialize
-    max_rows = 100;
+    max_rows = 1000;
     IF rowlimit > max_rows THEN
         RAISE 'rowlimit skal være <= %', max_rows;
     END IF;
