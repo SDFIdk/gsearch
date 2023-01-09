352c32621be0b69ad94d2a01b3977fa0bc55bd75
JesperKock <jekoc@sdfe.dk>
2023-01-09 15:19:16 +0100

    Change postdistrik to postnummer

--- db/seed/240_api_postnummer.sql
+++ db/seed/240_api_postnummer.sql
@@ -1,11 +1,11 @@
-SELECT '240_api_postdistrikt.sql ' || now();
+SELECT '240_api_postnummer.sql ' || now();
 
 
 CREATE SCHEMA IF NOT EXISTS api;
 
-DROP TYPE IF EXISTS api.postdistrikt CASCADE;
+DROP TYPE IF EXISTS api.postnummer CASCADE;
 
-CREATE TYPE api.postdistrikt AS (
+CREATE TYPE api.postnummer AS (
     postnummer text,
     postnummernavn text,
     visningstekst text,
@@ -16,21 +16,21 @@ CREATE TYPE api.postdistrikt AS (
     rang2 double precision
 );
 
-COMMENT ON TYPE api.postdistrikt IS 'Postdistrikt';
+COMMENT ON TYPE api.postnummer IS 'Postdistrikt';
 
-COMMENT ON COLUMN api.postdistrikt.postnummer IS 'Postnummer';
+COMMENT ON COLUMN api.postnummer.postnummer IS 'Postnummer';
 
-COMMENT ON COLUMN api.postdistrikt.postnummernavn IS 'Navn på postdistrikt';
+COMMENT ON COLUMN api.postnummer.postnummernavn IS 'Navn på postdistrikt';
 
-COMMENT ON COLUMN api.postdistrikt.visningstekst IS 'Præsentationsform for et postdistrikt';
+COMMENT ON COLUMN api.postnummer.visningstekst IS 'Præsentationsform for et postdistrikt';
 
-COMMENT ON COLUMN api.postdistrikt.gadepostnummer IS 'Dækker postnummeret kun en gade';
+COMMENT ON COLUMN api.postnummer.gadepostnummer IS 'Dækker postnummeret kun en gade';
 
-COMMENT ON COLUMN api.postdistrikt.geometri IS 'Geometri i valgt koordinatsystem';
+COMMENT ON COLUMN api.postnummer.geometri IS 'Geometri i valgt koordinatsystem';
 
-COMMENT ON COLUMN api.postdistrikt.bbox IS 'Geometriens boundingbox i valgt koordinatsystem';
+COMMENT ON COLUMN api.postnummer.bbox IS 'Geometriens boundingbox i valgt koordinatsystem';
 
-DROP TABLE IF EXISTS basic.postdistrikt;
+DROP TABLE IF EXISTS basic.postnummer;
 
 WITH postnumre AS (
     SELECT
@@ -48,7 +48,7 @@ SELECT
     (p.ergadepostnummer = 'true') AS ergadepostnummer,
     st_multi (st_union (p.geometri)) AS geometri,
     st_extent (p.geometri) AS bbox,
-    array_agg(k.kommunekode) AS kommunekoder INTO basic.postdistrikt
+    array_agg(k.kommunekode) AS kommunekoder INTO basic.postnummer
 FROM
     postnumre p
     LEFT JOIN dagi_500.kommuneinddeling k ON (st_intersects (k.geometri, p.geometri))
@@ -57,34 +57,34 @@ GROUP BY
     p.postnummernavn,
     p.ergadepostnummer;
 
-ALTER TABLE basic.postdistrikt
+ALTER TABLE basic.postnummer
     DROP COLUMN IF EXISTS textsearchable_plain_col;
 
-ALTER TABLE basic.postdistrikt
+ALTER TABLE basic.postnummer
     ADD COLUMN textsearchable_plain_col tsvector GENERATED ALWAYS AS (setweight(to_tsvector('simple', postnummer), 'A') || setweight(to_tsvector('simple', split_part(postnummernavn, ' ', 1)), 'B') || setweight(to_tsvector('simple', split_part(postnummernavn, ' ', 2)), 'C') || setweight(to_tsvector('simple', basic.split_and_endsubstring (postnummernavn, 3)), 'D')) STORED;
 
-ALTER TABLE basic.postdistrikt
+ALTER TABLE basic.postnummer
     DROP COLUMN IF EXISTS textsearchable_unaccent_col;
 
-ALTER TABLE basic.postdistrikt
+ALTER TABLE basic.postnummer
     ADD COLUMN textsearchable_unaccent_col tsvector GENERATED ALWAYS AS (setweight(to_tsvector('basic.septima_fts_config', postnummer), 'A') || setweight(to_tsvector('basic.septima_fts_config', split_part(postnummernavn, ' ', 1)), 'B') || setweight(to_tsvector('basic.septima_fts_config', split_part(postnummernavn, ' ', 2)), 'C') || setweight(to_tsvector('basic.septima_fts_config', basic.split_and_endsubstring (postnummernavn, 3)), 'D')) STORED;
 
-ALTER TABLE basic.postdistrikt
+ALTER TABLE basic.postnummer
     DROP COLUMN IF EXISTS textsearchable_phonetic_col;
 
-ALTER TABLE basic.postdistrikt
+ALTER TABLE basic.postnummer
     ADD COLUMN textsearchable_phonetic_col tsvector GENERATED ALWAYS AS (setweight(to_tsvector('simple', postnummer), 'A') || setweight(to_tsvector('simple', fonetik.fnfonetik (split_part(postnummernavn, ' ', 1), 2)), 'B') || setweight(to_tsvector('simple', fonetik.fnfonetik (split_part(postnummernavn, ' ', 2), 2)), 'C') || setweight(to_tsvector('simple', basic.split_and_endsubstring_fonetik (postnummernavn, 3)), 'D')) STORED;
 
-CREATE INDEX ON basic.postdistrikt USING GIN (textsearchable_plain_col);
+CREATE INDEX ON basic.postnummer USING GIN (textsearchable_plain_col);
 
-CREATE INDEX ON basic.postdistrikt USING GIN (textsearchable_unaccent_col);
+CREATE INDEX ON basic.postnummer USING GIN (textsearchable_unaccent_col);
 
-CREATE INDEX ON basic.postdistrikt USING GIN (textsearchable_phonetic_col);
+CREATE INDEX ON basic.postnummer USING GIN (textsearchable_phonetic_col);
 
-DROP FUNCTION IF EXISTS api.postdistrikt (text, jsonb, int, int);
+DROP FUNCTION IF EXISTS api.postnummer (text, jsonb, int, int);
 
-CREATE OR REPLACE FUNCTION api.postdistrikt (input_tekst text, filters text, sortoptions integer, rowlimit integer)
-    RETURNS SETOF api.postdistrikt
+CREATE OR REPLACE FUNCTION api.postnummer (input_tekst text, filters text, sortoptions integer, rowlimit integer)
+    RETURNS SETOF api.postnummer
     LANGUAGE plpgsql
     STABLE
     AS $function$
@@ -92,7 +92,7 @@ DECLARE
     max_rows integer;
     input_postnummernavn text;
     input_postnummer text;
-    postdistrikt_string text;
+    postnummer_string text;
     postnummernavn_string_plain text;
     postnummer_string text;
     query_string text;
@@ -128,7 +128,7 @@ BEGIN
     SELECT
         string_agg(fonetik.fnfonetik (t, 2), ':BCD* <-> ') || ':BCD*'
     FROM
-        tokens INTO postdistrikt_string;
+        tokens INTO postnummer_string;
     WITH tokens AS (
         SELECT
             UNNEST(string_to_array(input_postnummernavn, ' ')) t
@@ -145,18 +145,18 @@ BEGIN
         string_agg(t, ':A | ') || ':A'
     FROM
         tokens INTO postnummer_string;
-    --RAISE NOTICE 'postdistrikt_string: %', postdistrikt_string;
+    --RAISE NOTICE 'postnummer_string: %', postdistrikt_string;
     --RAISE NOTICE 'postnummernavn_string_plain: %', postnummernavn_string_plain;
     --RAISE NOTICE 'postnummer_string: %', postnummer_string;
-    CASE WHEN postdistrikt_string IS NULL THEN
+    CASE WHEN postnummer_string IS NULL THEN
         SELECT
             postnummer_string INTO query_string;
     WHEN postnummer_string IS NULL THEN
         SELECT
-            postdistrikt_string INTO query_string;
+            postnummer_string INTO query_string;
         ELSE
             SELECT
-                postdistrikt_string || ' | ' || postnummer_string INTO query_string;
+                postnummer_string || ' | ' || postnummer_string INTO query_string;
     END CASE;
     CASE WHEN postnummernavn_string_plain IS NULL THEN
         SELECT
@@ -175,7 +175,7 @@ BEGIN
             basic.combine_rank($2, $2, textsearchable_plain_col, textsearchable_unaccent_col, ''simple''::regconfig, ''basic.septima_fts_config''::regconfig) AS rang1,
             ts_rank_cd(textsearchable_phonetic_col, to_tsquery(''simple'',$1))::double precision AS rang2
             FROM
-            basic.postdistrikt
+            basic.postnummer
             WHERE (
                 textsearchable_phonetic_col @@ to_tsquery(''simple'', $1)
                 OR textsearchable_plain_col @@ to_tsquery(''simple'', $2))
@@ -190,26 +190,26 @@ BEGIN
 $function$;
 -- Test cases:
 /*
- SELECT (api.postdistrikt('lyn',NULL, 1, 100)).*;
- SELECT (api.postdistrikt('københavn s',NULL, 1, 100)).*;
- SELECT (api.postdistrikt('københavn k',NULL, 1, 100)).*;
- SELECT (api.postdistrikt('sø',NULL, 1, 100)).*;
- SELECT (api.postdistrikt('2300 søborg',NULL, 1, 100)).*;
- SELECT (api.postdistrikt('århus v',NULL, 1, 100)).*;
- SELECT (api.postdistrikt('ålborg øs',NULL, 1, 100)).*;
- SELECT (api.postdistrikt('køben ø',NULL, 1, 100)).*;
- SELECT (api.postdistrikt('Age',NULL, 1, 100)).*;
- SELECT (api.postdistrikt('.',NULL, 1, 100)).*;
- SELECT (api.postdistrikt(null,NULL, 1, 100)).*;
+ SELECT (api.postnummer('lyn',NULL, 1, 100)).*;
+ SELECT (api.postnummer('københavn s',NULL, 1, 100)).*;
+ SELECT (api.postnummer('københavn k',NULL, 1, 100)).*;
+ SELECT (api.postnummer('sø',NULL, 1, 100)).*;
+ SELECT (api.postnummer('2300 søborg',NULL, 1, 100)).*;
+ SELECT (api.postnummer('århus v',NULL, 1, 100)).*;
+ SELECT (api.postnummer('ålborg øs',NULL, 1, 100)).*;
+ SELECT (api.postnummer('køben ø',NULL, 1, 100)).*;
+ SELECT (api.postnummer('Age',NULL, 1, 100)).*;
+ SELECT (api.postnummer('.',NULL, 1, 100)).*;
+ SELECT (api.postnummer(null,NULL, 1, 100)).*;
 
- SELECT	(api.postdistrikt('a 1 a',	NULL,	1,	100)).*;
+ SELECT	(api.postnummer('a 1 a',	NULL,	1,	100)).*;
 
- SELECT	(api.postdistrikt('Anholt 8961',	NULL,	1,	100)).*;
+ SELECT	(api.postnummer('Anholt 8961',	NULL,	1,	100)).*;
 
- SELECT	(api.postdistrikt('6753 Lyngby',	NULL,	1,	100)).*;
+ SELECT	(api.postnummer('6753 Lyngby',	NULL,	1,	100)).*;
 
-SELECT	(api.postdistrikt('Anholt Ans By Ansager Arden Asaa',	NULL,	1,	100)).*;
+SELECT	(api.postnummer('Anholt Ans By Ansager Arden Asaa',	NULL,	1,	100)).*;
 
-SELECT	(api.postdistrikt('5320 6753 6534 4244 2620 3450 3770 8961',	NULL,	1,	100)).*;
+SELECT	(api.postnummer('5320 6753 6534 4244 2620 3450 3770 8961',	NULL,	1,	100)).*;
 
  */
