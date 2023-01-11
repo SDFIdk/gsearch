f8b6f162ca2a13ef1dc066055108cae648e62ee5
JesperKock <jekoc@sdfe.dk>
2023-01-11 16:16:27 +0100

    Improve code readablility

--- db/seed/220_api_opstillingskreds.sql
+++ db/seed/220_api_opstillingskreds.sql
@@ -72,19 +72,34 @@ ALTER TABLE basic.opstillingskreds
     DROP COLUMN IF EXISTS textsearchable_plain_col;
 
 ALTER TABLE basic.opstillingskreds
-    ADD COLUMN textsearchable_plain_col tsvector GENERATED ALWAYS AS (setweight(to_tsvector('simple', split_part(opstillingskredsnavn, ' ', 1)), 'A') || setweight(to_tsvector('simple', split_part(opstillingskredsnavn, ' ', 2)), 'B') || setweight(to_tsvector('simple', split_part(opstillingskredsnavn, ' ', 3)), 'C') || setweight(to_tsvector('simple', basic.split_and_endsubstring (opstillingskredsnavn, 4)), 'D')) STORED;
+    ADD COLUMN textsearchable_plain_col tsvector
+    GENERATED ALWAYS AS (setweight(to_tsvector('simple', split_part(opstillingskredsnavn, ' ', 1)), 'A') ||
+                         setweight(to_tsvector('simple', split_part(opstillingskredsnavn, ' ', 2)), 'B') ||
+                         setweight(to_tsvector('simple', split_part(opstillingskredsnavn, ' ', 3)), 'C') ||
+                         setweight(to_tsvector('simple', basic.split_and_endsubstring (opstillingskredsnavn, 4)), 'D'))
+    STORED;
 
 ALTER TABLE basic.opstillingskreds
     DROP COLUMN IF EXISTS textsearchable_unaccent_col;
 
 ALTER TABLE basic.opstillingskreds
-    ADD COLUMN textsearchable_unaccent_col tsvector GENERATED ALWAYS AS (setweight(to_tsvector('basic.septima_fts_config', split_part(opstillingskredsnavn, ' ', 1)), 'A') || setweight(to_tsvector('basic.septima_fts_config', split_part(opstillingskredsnavn, ' ', 2)), 'B') || setweight(to_tsvector('basic.septima_fts_config', split_part(opstillingskredsnavn, ' ', 3)), 'C') || setweight(to_tsvector('basic.septima_fts_config', basic.split_and_endsubstring (opstillingskredsnavn, 4)), 'D')) STORED;
+    ADD COLUMN textsearchable_unaccent_col tsvector
+    GENERATED ALWAYS AS (setweight(to_tsvector('basic.septima_fts_config', split_part(opstillingskredsnavn, ' ', 1)), 'A') ||
+                         setweight(to_tsvector('basic.septima_fts_config', split_part(opstillingskredsnavn, ' ', 2)), 'B') ||
+                         setweight(to_tsvector('basic.septima_fts_config', split_part(opstillingskredsnavn, ' ', 3)), 'C') ||
+                         setweight(to_tsvector('basic.septima_fts_config', basic.split_and_endsubstring (opstillingskredsnavn, 4)), 'D'))
+    STORED;
 
 ALTER TABLE basic.opstillingskreds
     DROP COLUMN IF EXISTS textsearchable_phonetic_col;
 
 ALTER TABLE basic.opstillingskreds
-    ADD COLUMN textsearchable_phonetic_col tsvector GENERATED ALWAYS AS (setweight(to_tsvector('simple', fonetik.fnfonetik (split_part(opstillingskredsnavn, ' ', 1), 2)), 'A') || setweight(to_tsvector('simple', fonetik.fnfonetik (split_part(opstillingskredsnavn, ' ', 2), 2)), 'B') || setweight(to_tsvector('simple', fonetik.fnfonetik (split_part(opstillingskredsnavn, ' ', 3), 2)), 'C') || setweight(to_tsvector('simple', basic.split_and_endsubstring_fonetik (coalesce(opstillingskredsnavn), 4)), 'D')) STORED;
+    ADD COLUMN textsearchable_phonetic_col tsvector
+    GENERATED ALWAYS AS (setweight(to_tsvector('simple', fonetik.fnfonetik (split_part(opstillingskredsnavn, ' ', 1), 2)), 'A') ||
+                         setweight(to_tsvector('simple', fonetik.fnfonetik (split_part(opstillingskredsnavn, ' ', 2), 2)), 'B') ||
+                         setweight(to_tsvector('simple', fonetik.fnfonetik (split_part(opstillingskredsnavn, ' ', 3), 2)), 'C') ||
+                         setweight(to_tsvector('simple', basic.split_and_endsubstring_fonetik (coalesce(opstillingskredsnavn), 4)), 'D'))
+    STORED;
 
 CREATE INDEX ON basic.opstillingskreds USING GIN (textsearchable_plain_col);
 
@@ -97,6 +112,7 @@ DROP FUNCTION IF EXISTS api.opstillingskreds (text, jsonb, int, int);
 CREATE OR REPLACE FUNCTION api.opstillingskreds (input_tekst text, filters text, sortoptions integer, rowlimit integer)
     RETURNS SETOF api.opstillingskreds
     LANGUAGE plpgsql
+
     STABLE
     AS $function$
 DECLARE
--- db/seed/310_api_navngivenvej.sql
+++ db/seed/310_api_navngivenvej.sql
@@ -80,21 +80,36 @@ ALTER TABLE basic.navngivenvej
     DROP COLUMN IF EXISTS textsearchable_plain_col;
 
 ALTER TABLE basic.navngivenvej
-    ADD COLUMN textsearchable_plain_col tsvector GENERATED ALWAYS AS (setweight(to_tsvector('simple', split_part(vejnavn, ' ', 1)), 'A') || setweight(to_tsvector('simple', split_part(vejnavn, ' ', 2)), 'B') || setweight(to_tsvector('simple', split_part(vejnavn, ' ', 3)), 'C') || setweight(to_tsvector('simple', basic.split_and_endsubstring (vejnavn, 4)), 'D')) STORED;
+    ADD COLUMN textsearchable_plain_col tsvector
+    GENERATED ALWAYS AS (setweight(to_tsvector('simple', split_part(vejnavn, ' ', 1)), 'A') ||
+                         setweight(to_tsvector('simple', split_part(vejnavn, ' ', 2)), 'B') ||
+                         setweight(to_tsvector('simple', split_part(vejnavn, ' ', 3)), 'C') ||
+                         setweight(to_tsvector('simple', basic.split_and_endsubstring (vejnavn, 4)), 'D'))
+    STORED;
 
 -- unaccented textsearchable column: å -> aa, é -> e, ect.
 ALTER TABLE basic.navngivenvej
     DROP COLUMN IF EXISTS textsearchable_unaccent_col;
 
 ALTER TABLE basic.navngivenvej
-    ADD COLUMN textsearchable_unaccent_col tsvector GENERATED ALWAYS AS (setweight(to_tsvector('basic.septima_fts_config', split_part(vejnavn, ' ', 1)), 'A') || setweight(to_tsvector('basic.septima_fts_config', split_part(vejnavn, ' ', 2)), 'B') || setweight(to_tsvector('basic.septima_fts_config', split_part(vejnavn, ' ', 3)), 'C') || setweight(to_tsvector('basic.septima_fts_config', basic.split_and_endsubstring (vejnavn, 4)), 'D')) STORED;
+    ADD COLUMN textsearchable_unaccent_col tsvector
+    GENERATED ALWAYS AS (setweight(to_tsvector('basic.septima_fts_config', split_part(vejnavn, ' ', 1)), 'A') ||
+                         setweight(to_tsvector('basic.septima_fts_config', split_part(vejnavn, ' ', 2)), 'B') ||
+                         setweight(to_tsvector('basic.septima_fts_config', split_part(vejnavn, ' ', 3)), 'C') ||
+                         setweight(to_tsvector('basic.septima_fts_config', basic.split_and_endsubstring (vejnavn, 4)), 'D'))
+    STORED;
 
 -- phonetic textsearchable column: christian -> kristian, k
 ALTER TABLE basic.navngivenvej
     DROP COLUMN IF EXISTS textsearchable_phonetic_col;
 
 ALTER TABLE basic.navngivenvej
-    ADD COLUMN textsearchable_phonetic_col tsvector GENERATED ALWAYS AS (setweight(to_tsvector('simple', fonetik.fnfonetik (split_part(vejnavn, ' ', 1), 2)), 'A') || setweight(to_tsvector('simple', fonetik.fnfonetik (split_part(vejnavn, ' ', 2), 2)), 'B') || setweight(to_tsvector('simple', fonetik.fnfonetik (split_part(vejnavn, ' ', 3), 2)), 'C') || setweight(to_tsvector('simple', basic.split_and_endsubstring_fonetik (vejnavn, 4)), 'D')) STORED;
+    ADD COLUMN textsearchable_phonetic_col tsvector
+    GENERATED ALWAYS AS (setweight(to_tsvector('simple', fonetik.fnfonetik (split_part(vejnavn, ' ', 1), 2)), 'A') ||
+                         setweight(to_tsvector('simple', fonetik.fnfonetik (split_part(vejnavn, ' ', 2), 2)), 'B') ||
+                         setweight(to_tsvector('simple', fonetik.fnfonetik (split_part(vejnavn, ' ', 3), 2)), 'C') ||
+                         setweight(to_tsvector('simple', basic.split_and_endsubstring_fonetik (vejnavn, 4)), 'D'))
+    STORED;
 
 CREATE INDEX ON basic.navngivenvej USING GIN (textsearchable_plain_col);
 
