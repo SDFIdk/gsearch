3c52015fc505f7a91253b3a6a0b3cb5778652918
Nobralyra <48592380+Nobralyra@users.noreply.github.com>
2023-01-17 10:48:27 +0100

    Revert "Try to remove rang1 and rang 2 from type"
    This reverts commit 49d3575e29e1d9b8b39fd6e4409161759959cf16.

--- db/seed/210_api_kommune.sql
+++ db/seed/210_api_kommune.sql
@@ -10,7 +10,9 @@ CREATE TYPE api.kommune AS (
     kommunenavn text,
     visningstekst text,
     geometri geometry,
-    bbox geometry
+    bbox geometry,
+    rang1 double precision,
+    rang2 double precision
 );
 
 COMMENT ON TYPE api.kommune IS 'Kommune';
@@ -179,8 +181,8 @@ BEGIN
     -- Execute and return the result
     stmt = format(E'SELECT
             kommunekode::text, kommunenavn::text, visningstekst, geometri, bbox::geometry,
-            basic.combine_rank($2, $2, textsearchable_plain_col, textsearchable_unaccent_col, ''simple''::regconfig, ''basic.septima_fts_config''::regconfig),
-            ts_rank_cd(textsearchable_phonetic_col, to_tsquery(''simple'',$1))::double precision
+            basic.combine_rank($2, $2, textsearchable_plain_col, textsearchable_unaccent_col, ''simple''::regconfig, ''basic.septima_fts_config''::regconfig) AS rang1,
+            ts_rank_cd(textsearchable_phonetic_col, to_tsquery(''simple'',$1))::double precision AS rang2
             FROM
             basic.kommune
             WHERE (
@@ -188,8 +190,7 @@ BEGIN
                 OR textsearchable_plain_col @@ to_tsquery(''simple'', $2) )
             AND %s
             ORDER BY
-            basic.combine_rank($2, $2, textsearchable_plain_col, textsearchable_unaccent_col, ''simple''::regconfig, ''basic.septima_fts_config''::regconfig) desc,
-            ts_rank_cd(textsearchable_phonetic_col, to_tsquery(''simple'',$1))::double precision desc,
+            rang1 desc, rang2 desc,
             kommunenavn
             LIMIT $3
             ;', filters); RETURN QUERY EXECUTE stmt
