SDFIdk/gsearch

commit a3a9fa9a3b1200e43f67dddcc8d2c557ebb70b6d

Add regex to remove excess whitespace Adresses #170

db/seed/210_api_kommune.sql
@@ -106,6 +106,7 @@ DECLARE
     max_rows integer;
     input_kommunenavn text;
     input_kommunekode text;
+    input_kommunenavn_fonetik text;
     kommunenavn_string text;
     kommunenavn_string_plain text;
     kommunekode_string text;
@@ -125,25 +126,35 @@ BEGIN
     IF btrim(input_tekst) = ANY ('{.,-, '', \,}') THEN
         input_tekst = '';
     END IF;
+
     SELECT
         -- matches non-digits
-        btrim((REGEXP_MATCH(btrim(input_tekst), '([^\d]+)'))[1])
+       regexp_replace(btrim((REGEXP_MATCH(btrim(input_tekst), '([^\d]+)'))[1]), '\s+', ' ', 'g')
     INTO input_kommunenavn;
+
     SELECT
         -- Removes everything that starts with a letter or symbol (not digits) and then removes repeated whitespace.
-        btrim(regexp_replace(regexp_replace(input_tekst, '((?<!\S)\D\S*)', '', 'g'), '\s+', ' ')) INTO input_kommunekode;
-    WITH tokens AS (
-        SELECT
-            UNNEST(string_to_array(input_kommunenavn, ' ')) t
-)
+        regexp_replace(btrim(regexp_replace(regexp_replace(input_tekst, '((?<!\S)\D\S*)', '', 'g'), '\s+', ' ')) , '\s+', ' ', 'g')
+    INTO input_kommunekode;
+
     SELECT
-        string_agg(fonetik.fnfonetik (t, 2), ':BCD* <-> ') || ':BCD*'
-    FROM
-        tokens INTO kommunenavn_string;
+        regexp_replace(fonetik.fnfonetik (input_kommunenavn, 2), '\s+', ' ', 'g')
+    INTO input_kommunenavn_fonetik;
+
     WITH tokens AS (
         SELECT
             UNNEST(string_to_array(input_kommunenavn, ' ')) t
-)
+            )
+    SELECT
+        string_agg(input_kommunenavn_fonetik;, ':BCD* <-> ') || ':BCD*'
+    FROM
+        tokens
+    INTO kommunenavn_string;
+
+    WITH tokens AS (
+        SELECT
+            UNNEST(string_to_array(input_kommunenavn, ' ')) t
+            )
     SELECT
         string_agg(t, ':BCD* <-> ') || ':BCD*'
     FROM
@@ -151,7 +162,7 @@ BEGIN
     WITH tokens AS (
         SELECT
             UNNEST(string_to_array(input_kommunekode, ' ')) t
-)
+            )
     SELECT
         string_agg(t, ':A | ') || ':A'
     FROM

db/seed/220_api_opstillingskreds.sql
@@ -114,6 +114,7 @@ CREATE OR REPLACE FUNCTION api.opstillingskreds (input_tekst text, filters text,
     STABLE
     AS $function$
 DECLARE
+    input_fonetik text;
     max_rows integer;
     query_string text;
     plain_query_string text;
@@ -130,13 +131,22 @@ BEGIN
     IF btrim(input_tekst) = ANY ('{.,-, '', \,}') THEN
         input_tekst = '';
     END IF;
+
+    SELECT
+        regexp_replace(input_tekst, '\s+', ' ', 'g')
+    INTO input_tekst;
+
+    SELECT
+        regexp_replace(fonetik.fnfonetik (input_tekst, 2), '\s+', ' ', 'g')
+    INTO input_fonetik;
+
     -- Build the query_string
     WITH tokens AS (
         SELECT
             UNNEST(string_to_array(btrim(input_tekst), ' ')) t
 )
     SELECT
-        string_agg(fonetik.fnfonetik (t, 2), ':* <-> ') || ':*'
+        string_agg(input_fonetik, ':* <-> ') || ':*'
     FROM
         tokens INTO query_string;
     -- build the plain version of the query string for ranking purposes

db/seed/230_api_politikreds.sql
@@ -100,6 +100,7 @@ CREATE OR REPLACE FUNCTION api.politikreds (input_tekst text, filters text, sort
     STABLE
     AS $function$
 DECLARE
+    input_fonetik text;
     max_rows integer;
     query_string text;
     plain_query_string text;
@@ -117,12 +118,21 @@ BEGIN
         input_tekst = '';
     END IF;
     -- Build the query_string
+
+    SELECT
+        regexp_replace(input_tekst, '\s+', ' ', 'g')
+    INTO input;
+
+    SELECT
+        regexp_replace(fonetik.fnfonetik (input_tekst, 2), '\s+', ' ', 'g')
+    INTO input_fonetik;
+
     WITH tokens AS (
         SELECT
             UNNEST(string_to_array(btrim(input_tekst), ' ')) t
 )
     SELECT
-        string_agg(fonetik.fnfonetik (t, 2), ':* <-> ') || ':*'
+        string_agg(input_fonetik, ':* <-> ') || ':*'
     FROM
         tokens INTO query_string;
     -- build the plain version of the query string for ranking purposes

db/seed/240_api_postnummer.sql
@@ -104,6 +104,7 @@ CREATE OR REPLACE FUNCTION api.postnummer (input_tekst text, filters text, sorto
 DECLARE
     max_rows integer;
     input_postnummernavn text;
+    input_postnummernavn_fonetik text;
     input_postnummer text;
     postnummernavn_string text;
     postnummernavn_string_plain text;
@@ -124,22 +125,30 @@ BEGIN
     IF btrim(input_tekst) = ANY ('{.,-, '', \,}') THEN
         input_tekst = '';
     END IF;
+
     SELECT
         -- matches non-digits
-        btrim((REGEXP_MATCH(btrim(input_tekst), '([^\d]+)'))[1])
+        regexp_replace(btrim((REGEXP_MATCH(btrim(input_tekst), '([^\d]+)'))[1]), '\s+', ' ', 'g')
     INTO input_postnummernavn;
+
+    SELECT
+        regexp_replace(fonetik.fnfonetik (input_postnummernavn, 2), '\s+', ' ', 'g')
+    INTO input_postnummernavn_fonetik;
+
     SELECT
         -- Removes everything that starts with a letter or symbol (not digits) and then removes repeated whitespace.
         btrim(regexp_replace(regexp_replace(input_tekst, '((?<!\S)\D\S*)', '', 'g'), '\s+', ' '))
     INTO input_postnummer;
+
     --RAISE NOTICE 'input_postnummernavn: %', input_postnummernavn;
     --RAISE NOTICE 'input_postnummer: %', input_postnummer;
+
     WITH tokens AS (
         SELECT
             UNNEST(string_to_array(input_postnummernavn, ' ')) t
 )
     SELECT
-        string_agg(fonetik.fnfonetik (t, 2), ':BCD* <-> ') || ':BCD*'
+        string_agg(input_postnummernavn_fonetik (t, 2), ':BCD* <-> ') || ':BCD*'
     FROM
         tokens INTO postnummernavn_string;
     WITH tokens AS (

db/seed/250_api_region.sql
@@ -95,6 +95,7 @@ CREATE OR REPLACE FUNCTION api.region (input_tekst text, filters text, sortoptio
     AS $function$
 DECLARE
     max_rows integer;
+    input_fonetik text;
     query_string text;
     plain_query_string text;
     stmt text;
@@ -110,20 +111,29 @@ BEGIN
     IF btrim(input_tekst) = ANY ('{.,-, '', \,}') THEN
         input_tekst = '';
     END IF;
+
+    SELECT
+        regexp_replace(input_tekst, '\s+', ' ', 'g')
+    INTO input_tekst;
+
+    SELECT
+        regexp_replace(fonetik.fnfonetik (input_tekst, 2), '\s+', ' ', 'g')
+    INTO input_fonetik;
+
     -- Build the query_string
     WITH tokens AS (
         SELECT
             UNNEST(string_to_array(btrim(input_tekst), ' ')) t
-)
+            )
     SELECT
-        string_agg(fonetik.fnfonetik (t, 2), ':* <-> ') || ':*'
+        string_agg(input_fonetik, ':* <-> ') || ':*'
     FROM
         tokens INTO query_string;
     -- build the plain version of the query string for ranking purposes
     WITH tokens AS (
         SELECT
             UNNEST(string_to_array(btrim(input_tekst), ' ')) t
-)
+            )
     SELECT
         string_agg(t, ':* <-> ') || ':*'
     FROM

db/seed/260_api_retskreds.sql
@@ -100,6 +100,7 @@ CREATE OR REPLACE FUNCTION api.retskreds (input_tekst text, filters text, sortop
     STABLE
     AS $function$
 DECLARE
+    input_fonetik text;
     max_rows integer;
     query_string text;
     plain_query_string text;
@@ -116,13 +117,22 @@ BEGIN
     IF btrim(input_tekst) = ANY ('{.,-, '', \,}') THEN
         input_tekst = '';
     END IF;
+
+    SELECT
+        regexp_replace(input_tekst, '\s+', ' ', 'g')
+    INTO input_tekst;
+
+    SELECT
+        regexp_replace(fonetik.fnfonetik (input_tekst, 2), '\s+', ' ', 'g')
+    INTO input_fonetik;
+
     -- Build the query_string
     WITH tokens AS (
         SELECT
             UNNEST(string_to_array(btrim(input_tekst), ' ')) t
 )
     SELECT
-        string_agg(fonetik.fnfonetik (t, 2), ':* <-> ') || ':*'
+        string_agg(input_fonetik, ':* <-> ') || ':*'
     FROM
         tokens INTO query_string;
     -- build the plain version of the query string for ranking purposes

db/seed/270_api_sogn.sql
@@ -94,6 +94,7 @@ CREATE OR REPLACE FUNCTION api.sogn (input_tekst text, filters text, sortoptions
     STABLE
     AS $function$
 DECLARE
+    input_fonetik text;
     max_rows integer;
     query_string text;
     plain_query_string text;
@@ -110,13 +111,22 @@ BEGIN
     IF btrim(input_tekst) = ANY ('{.,-, '', \,}') THEN
         input_tekst = '';
     END IF;
+
+    SELECT
+        regexp_replace(input_tekst, '\s+', ' ', 'g')
+    INTO input;
+
+    SELECT
+        regexp_replace(fonetik.fnfonetik (input_tekst, 2), '\s+', ' ', 'g')
+    INTO input_fonetik;
+
     -- Build the query_string
     WITH tokens AS (
         SELECT
             UNNEST(string_to_array(btrim(input_tekst), ' ')) t
 )
     SELECT
-        string_agg(fonetik.fnfonetik (t, 2), ':* <-> ') || ':*'
+        string_agg(input_fonetik, ':* <-> ') || ':*'
     FROM
         tokens INTO query_string;
     -- build the plain version of the query string for ranking purposes

db/seed/310_api_navngivenvej.sql
@@ -188,6 +188,7 @@ CREATE OR REPLACE FUNCTION api.navngivenvej (input_tekst text, filters text, sor
     STABLE
     AS $function$
 DECLARE
+    input_fonetik text;
     max_rows integer;
     query_string text;
     plain_query_string text;
@@ -204,6 +205,15 @@ BEGIN
     IF btrim(input_tekst) = ANY ('{.,-, '', \,}') THEN
         input_tekst = '';
     END IF;
+
+    SELECT
+        regexp_replace(input_tekst, '\s+', ' ', 'g')
+    INTO input_tekst;
+
+    SELECT
+        regexp_replace(fonetik.fnfonetik (input_tekst, 2), '\s+', ' ', 'g')
+    INTO input_fonetik;
+
     -- Build the query_string
     WITH tokens AS (
         SELECT

commit 83b9af5d346ddbda0a9c374a1e0dab45ed7f310c

Fix phonetik bug introduced with a3a9fa9a

db/seed/210_api_kommune.sql
@@ -106,7 +106,6 @@ DECLARE
     max_rows integer;
     input_kommunenavn text;
     input_kommunekode text;
-    input_kommunenavn_fonetik text;
     kommunenavn_string text;
     kommunenavn_string_plain text;
     kommunekode_string text;
@@ -129,7 +128,7 @@ BEGIN
 
     SELECT
         -- matches non-digits
-       regexp_replace(btrim((REGEXP_MATCH(btrim(input_tekst), '([^\d]+)'))[1]), '\s+', ' ', 'g')
+       regexp_replace(btrim((REGEXP_MATCH(btrim(input_tekst), '([^\d]+)'))[1]), '[-.,\s]+', ' ', 'g')
     INTO input_kommunenavn;
 
     SELECT
@@ -137,16 +136,12 @@ BEGIN
         regexp_replace(btrim(regexp_replace(regexp_replace(input_tekst, '((?<!\S)\D\S*)', '', 'g'), '\s+', ' ')) , '\s+', ' ', 'g')
     INTO input_kommunekode;
 
-    SELECT
-        regexp_replace(fonetik.fnfonetik (input_kommunenavn, 2), '\s+', ' ', 'g')
-    INTO input_kommunenavn_fonetik;
-
     WITH tokens AS (
         SELECT
             UNNEST(string_to_array(input_kommunenavn, ' ')) t
             )
     SELECT
-        string_agg(input_kommunenavn_fonetik, ':BCD* <-> ') || ':BCD*'
+        string_agg(fonetik.fnfonetik (t, 2), ':BCD* <-> ') || ':BCD*'
     FROM
         tokens
     INTO kommunenavn_string;

db/seed/220_api_opstillingskreds.sql
@@ -114,7 +114,6 @@ CREATE OR REPLACE FUNCTION api.opstillingskreds (input_tekst text, filters text,
     STABLE
     AS $function$
 DECLARE
-    input_fonetik text;
     max_rows integer;
     query_string text;
     plain_query_string text;
@@ -133,12 +132,8 @@ BEGIN
     END IF;
 
     SELECT
-        regexp_replace(input_tekst, '\s+', ' ', 'g')
+        regexp_replace(input_tekst, '[-.,\s]+', ' ', 'g')
     INTO input_tekst;
-
-    SELECT
-        regexp_replace(fonetik.fnfonetik (input_tekst, 2), '\s+', ' ', 'g')
-    INTO input_fonetik;
 
     -- Build the query_string
     WITH tokens AS (
@@ -146,7 +141,7 @@ BEGIN
             UNNEST(string_to_array(btrim(input_tekst), ' ')) t
 )
     SELECT
-        string_agg(input_fonetik, ':* <-> ') || ':*'
+        string_agg(fonetik.fnfonetik (t, 2), ':* <-> ') || ':*'
     FROM
         tokens INTO query_string;
     -- build the plain version of the query string for ranking purposes

db/seed/230_api_politikreds.sql
@@ -100,7 +100,6 @@ CREATE OR REPLACE FUNCTION api.politikreds (input_tekst text, filters text, sort
     STABLE
     AS $function$
 DECLARE
-    input_fonetik text;
     max_rows integer;
     query_string text;
     plain_query_string text;
@@ -120,19 +119,15 @@ BEGIN
     -- Build the query_string
 
     SELECT
-        regexp_replace(input_tekst, '\s+', ' ', 'g')
+        regexp_replace(input_tekst, '[-.,\s]+', ' ', 'g')
     INTO input_tekst;
-
-    SELECT
-        regexp_replace(fonetik.fnfonetik (input_tekst, 2), '\s+', ' ', 'g')
-    INTO input_fonetik;
 
     WITH tokens AS (
         SELECT
             UNNEST(string_to_array(btrim(input_tekst), ' ')) t
 )
     SELECT
-        string_agg(input_fonetik, ':* <-> ') || ':*'
+        string_agg(fonetik.fnfonetik (t, 2), ':* <-> ') || ':*'
     FROM
         tokens INTO query_string;
     -- build the plain version of the query string for ranking purposes

db/seed/240_api_postnummer.sql
@@ -104,7 +104,6 @@ CREATE OR REPLACE FUNCTION api.postnummer (input_tekst text, filters text, sorto
 DECLARE
     max_rows integer;
     input_postnummernavn text;
-    input_postnummernavn_fonetik text;
     input_postnummer text;
     postnummernavn_string text;
     postnummernavn_string_plain text;
@@ -128,12 +127,8 @@ BEGIN
 
     SELECT
         -- matches non-digits
-        regexp_replace(btrim((REGEXP_MATCH(btrim(input_tekst), '([^\d]+)'))[1]), '\s+', ' ', 'g')
+        regexp_replace(btrim((REGEXP_MATCH(btrim(input_tekst), '([^\d]+)'))[1]), '[-.,\s]+', ' ', 'g')
     INTO input_postnummernavn;
-
-    SELECT
-        regexp_replace(fonetik.fnfonetik (input_postnummernavn, 2), '\s+', ' ', 'g')
-    INTO input_postnummernavn_fonetik;
 
     SELECT
         -- Removes everything that starts with a letter or symbol (not digits) and then removes repeated whitespace.
@@ -146,23 +141,25 @@ BEGIN
     WITH tokens AS (
         SELECT
             UNNEST(string_to_array(input_postnummernavn, ' ')) t
-)
+            )
     SELECT
-        string_agg(input_postnummernavn_fonetik, ':BCD* <-> ') || ':BCD*'
+        string_agg(fonetik.fnfonetik (t, 2), ':BCD* <-> ') || ':BCD*'
     FROM
         tokens INTO postnummernavn_string;
+
     WITH tokens AS (
         SELECT
             UNNEST(string_to_array(input_postnummernavn, ' ')) t
-)
+            )
     SELECT
         string_agg(t, ':BCD* <-> ') || ':BCD*'
     FROM
         tokens INTO postnummernavn_string_plain;
+
     WITH tokens AS (
         SELECT
             UNNEST(string_to_array(input_postnummer, ' ')) t
-)
+            )
     SELECT
         string_agg(t, ':A | ') || ':A'
     FROM

db/seed/250_api_region.sql
@@ -95,7 +95,6 @@ CREATE OR REPLACE FUNCTION api.region (input_tekst text, filters text, sortoptio
     AS $function$
 DECLARE
     max_rows integer;
-    input_fonetik text;
     query_string text;
     plain_query_string text;
     stmt text;
@@ -113,12 +112,8 @@ BEGIN
     END IF;
 
     SELECT
-        regexp_replace(input_tekst, '\s+', ' ', 'g')
+        regexp_replace(input_tekst, '[-.,\s]+', ' ', 'g')
     INTO input_tekst;
-
-    SELECT
-        regexp_replace(fonetik.fnfonetik (input_tekst, 2), '\s+', ' ', 'g')
-    INTO input_fonetik;
 
     -- Build the query_string
     WITH tokens AS (
@@ -126,7 +121,7 @@ BEGIN
             UNNEST(string_to_array(btrim(input_tekst), ' ')) t
             )
     SELECT
-        string_agg(input_fonetik, ':* <-> ') || ':*'
+        string_agg(fonetik.fnfonetik (t, 2), ':* <-> ') || ':*'
     FROM
         tokens INTO query_string;
     -- build the plain version of the query string for ranking purposes

db/seed/260_api_retskreds.sql
@@ -100,7 +100,6 @@ CREATE OR REPLACE FUNCTION api.retskreds (input_tekst text, filters text, sortop
     STABLE
     AS $function$
 DECLARE
-    input_fonetik text;
     max_rows integer;
     query_string text;
     plain_query_string text;
@@ -119,12 +118,8 @@ BEGIN
     END IF;
 
     SELECT
-        regexp_replace(input_tekst, '\s+', ' ', 'g')
+        regexp_replace(input_tekst, '[-.,\s]+', ' ', 'g')
     INTO input_tekst;
-
-    SELECT
-        regexp_replace(fonetik.fnfonetik (input_tekst, 2), '\s+', ' ', 'g')
-    INTO input_fonetik;
 
     -- Build the query_string
     WITH tokens AS (
@@ -132,7 +127,7 @@ BEGIN
             UNNEST(string_to_array(btrim(input_tekst), ' ')) t
 )
     SELECT
-        string_agg(input_fonetik, ':* <-> ') || ':*'
+        string_agg(fonetik.fnfonetik (t, 2), ':* <-> ') || ':*'
     FROM
         tokens INTO query_string;
     -- build the plain version of the query string for ranking purposes

db/seed/270_api_sogn.sql
@@ -94,7 +94,6 @@ CREATE OR REPLACE FUNCTION api.sogn (input_tekst text, filters text, sortoptions
     STABLE
     AS $function$
 DECLARE
-    input_fonetik text;
     max_rows integer;
     query_string text;
     plain_query_string text;
@@ -113,12 +112,8 @@ BEGIN
     END IF;
 
     SELECT
-        regexp_replace(input_tekst, '\s+', ' ', 'g')
+        regexp_replace(input_tekst, '[-.,\s]+', ' ', 'g')
     INTO input_tekst;
-
-    SELECT
-        regexp_replace(fonetik.fnfonetik (input_tekst, 2), '\s+', ' ', 'g')
-    INTO input_fonetik;
 
     -- Build the query_string
     WITH tokens AS (
@@ -126,7 +121,7 @@ BEGIN
             UNNEST(string_to_array(btrim(input_tekst), ' ')) t
 )
     SELECT
-        string_agg(input_fonetik, ':* <-> ') || ':*'
+        string_agg(fonetik.fnfonetik (t, 2), ':* <-> ') || ':*'
     FROM
         tokens INTO query_string;
     -- build the plain version of the query string for ranking purposes

db/seed/310_api_navngivenvej.sql
@@ -188,7 +188,6 @@ CREATE OR REPLACE FUNCTION api.navngivenvej (input_tekst text, filters text, sor
     STABLE
     AS $function$
 DECLARE
-    input_fonetik text;
     max_rows integer;
     query_string text;
     plain_query_string text;
@@ -207,12 +206,8 @@ BEGIN
     END IF;
 
     SELECT
-        regexp_replace(input_tekst, '\s+', ' ', 'g')
+        regexp_replace(input_tekst, '[-.,\s]+', ' ', 'g')
     INTO input_tekst;
-
-    SELECT
-        regexp_replace(fonetik.fnfonetik (input_tekst, 2), '\s+', ' ', 'g')
-    INTO input_fonetik;
 
     -- Build the query_string
     WITH tokens AS (
@@ -220,7 +215,7 @@ BEGIN
             UNNEST(string_to_array(btrim(input_tekst), ' ')) t
 )
     SELECT
-        string_agg(input_fonetik, ':* & ') || ':*'
+        string_agg(fonetik.fnfonetik (t, 2), ':* & ') || ':*'
     FROM
         tokens INTO query_string;
     -- build the plain version of the query string for ranking purposes

db/seed/320_api_adresse.sql
@@ -210,7 +210,6 @@ CREATE OR REPLACE FUNCTION api.adresse (input_tekst text, filters text, sortopti
 DECLARE
     max_rows integer;
     input text;
-    input_fonetik text;
     query_string text;
     plain_query_string text;
     stmt text;
@@ -227,14 +226,9 @@ BEGIN
         input_tekst = '';
     END IF;
 
-
     SELECT
-        regexp_replace(input_tekst, '\s+', ' ', 'g')
+        regexp_replace(input_tekst, '[-.,\s]+', ' ', 'g')
     INTO input;
-
-    SELECT
-        regexp_replace(fonetik.fnfonetik (input, 2), '\s+', ' ', 'g')
-    INTO input_fonetik;
 
     -- Build the query_string (converting vejnavn of input to phonetic)
     WITH tokens AS (
@@ -242,7 +236,7 @@ BEGIN
             UNNEST(string_to_array(btrim(input), ' ')) t
     )
     SELECT
-        string_agg(input_fonetik, ':* & ') || ':*'
+        string_agg(fonetik.fnfonetik (t, 2), ':* & ') || ':*'
     FROM
         tokens
     INTO query_string;

db/seed/330_api_husnummer.sql
@@ -190,7 +190,6 @@ CREATE OR REPLACE FUNCTION api.husnummer (input_tekst text, filters text, sortop
 DECLARE
     max_rows integer;
     input text;
-    input_fonetik text;
     query_string text;
     plain_query_string text;
     stmt text;
@@ -210,12 +209,8 @@ BEGIN
 
     -- Get vejnavn from input
     SELECT
-        regexp_replace(input_tekst, '\s+', ' ', 'g')
+        regexp_replace(input_tekst, '[-.,\s]+', ' ', 'g')
     INTO input;
-
-    SELECT
-        regexp_replace(fonetik.fnfonetik (input, 2), '\s+', ' ', 'g')
-    INTO input_fonetik;
 
     -- Build the query_string (converting vejnavn of input to phonetic)
     WITH tokens AS (
@@ -225,7 +220,7 @@ BEGIN
             UNNEST(string_to_array(btrim(input), ' ')) t
     )
     SELECT
-        string_agg(input_fonetik, ':* & ') || ':*'
+        string_agg(fonetik.fnfonetik (t, 2), ':* & ') || ':*'
     FROM
         tokens
     INTO query_string;

db/seed/410_api_matrikel.sql
@@ -157,7 +157,6 @@ CREATE OR REPLACE FUNCTION api.matrikel(input_tekst text, filters text, sortopti
 DECLARE
     max_rows integer;
     input_ejerlavsnavn text;
-    input_ejerlavsnavn_fonetik text;
     input_ejerlavskode_matrikel text;
     ejerlavsnavn_string text;
     ejerlavsnavn_string_plain text;
@@ -180,12 +179,8 @@ BEGIN
 
     SELECT
         -- matches non-digits
-        regexp_replace(btrim((REGEXP_MATCH(btrim(input_tekst), '([^\d]+)'))[1]), '\s+', ' ', 'g')
+        regexp_replace(btrim((REGEXP_MATCH(btrim(input_tekst), '([^\d]+)'))[1]), '[-.,\s]+', ' ', 'g')
     INTO input_ejerlavsnavn;
-
-    SELECT
-        regexp_replace(fonetik.fnfonetik (input_ejerlavsnavn, 2), '\s+', ' ', 'g')
-    INTO input_ejerlavsnavn_fonetik;
 
     SELECT
         -- Removes everything that starts with a letter or symbol (not digits) and then removes repeated whitespace.
@@ -207,7 +202,7 @@ BEGIN
             UNNEST(string_to_array(input_ejerlavsnavn, ' ')) t
     )
     SELECT
-        string_agg(input_ejerlavsnavn_fonetik, ':* <-> ') || ':*'
+        string_agg(fonetik.fnfonetik (t, 2), ':* <-> ') || ':*'
     FROM
         tokens INTO ejerlavsnavn_string;
 

db/seed/510_api_stednavne.sql
@@ -199,7 +199,6 @@ CREATE OR REPLACE FUNCTION api.stednavn (input_tekst text, filters text, sortopt
     STABLE
     AS $function$
 DECLARE
-    input_fonetik text;
     max_rows integer;
     query_string text;
     plain_query_string text;
@@ -218,19 +217,15 @@ BEGIN
     END IF;
 
     SELECT
-        regexp_replace(input_tekst, '\s+', ' ', 'g')
+        regexp_replace(input_tekst, '[-.,\s]+', ' ', 'g')
     INTO input_tekst;
-
-    SELECT
-        regexp_replace(fonetik.fnfonetik (input_tekst, 2), '\s+', ' ', 'g')
-    INTO input_fonetik;
 
     WITH tokens AS (
         SELECT
             UNNEST(string_to_array(btrim(replace(input_tekst, '-', ' ')), ' ')) t
     )
     SELECT
-            string_agg(input_fonetik, ':* <-> ') || ':*'
+            string_agg(fonetik.fnfonetik (t, 2), ':* <-> ') || ':*'
     FROM
         tokens
     INTO