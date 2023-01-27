10a9533c8be4e487972fdde202d6ddbe391a1071
Nobralyra <48592380+Nobralyra@users.noreply.github.com>
2023-01-27 14:46:16 +0100

    Formating

--- db/seed/410_api_matrikel.sql
+++ db/seed/410_api_matrikel.sql
@@ -283,41 +283,42 @@ BEGIN
         RETURN QUERY EXECUTE stmt
         USING query_string, plain_query_string, rowlimit;
     ELSE
-    -- Execute and return the result
-    stmt = format(E'SELECT
-            ejerlavsnavn::text,
-            ejerlavskode::text,
-            matrikelnummer::text,
-            visningstekst::text,
-            ST_X((ST_DUMP(centroide_geometri)).geom)::text,
-            ST_Y((ST_DUMP(centroide_geometri)).geom)::text,
-            geometri
-        FROM
-            basic.matrikel
-        WHERE (
-            textsearchable_phonetic_col @@ to_tsquery(''simple'', $1)
-            OR textsearchable_plain_col @@ to_tsquery(''simple'', $2)
-        )
-        AND %s
-        ORDER BY
-            basic.combine_rank(
-                $2,
-                $2,
-                textsearchable_plain_col,
-                textsearchable_unaccent_col,
-                ''simple''::regconfig,
-                ''basic.septima_fts_config''::regconfig
-            ) desc,
-            ts_rank_cd(
-                textsearchable_phonetic_col,
-                to_tsquery(''simple'',$1)
-            )::double precision desc,
-            matrikelnummer,
-            visningstekst
-        LIMIT $3  ;', filters); RETURN QUERY EXECUTE stmt
-    USING query_string, plain_query_string, rowlimit;
+        -- Execute and return the result
+        stmt = format(E'SELECT
+                ejerlavsnavn::text,
+                ejerlavskode::text,
+                matrikelnummer::text,
+                visningstekst::text,
+                ST_X((ST_DUMP(centroide_geometri)).geom)::text,
+                ST_Y((ST_DUMP(centroide_geometri)).geom)::text,
+                geometri
+            FROM
+                basic.matrikel
+            WHERE (
+                textsearchable_phonetic_col @@ to_tsquery(''simple'', $1)
+                OR textsearchable_plain_col @@ to_tsquery(''simple'', $2)
+            )
+            AND %s
+            ORDER BY
+                basic.combine_rank(
+                    $2,
+                    $2,
+                    textsearchable_plain_col,
+                    textsearchable_unaccent_col,
+                    ''simple''::regconfig,
+                    ''basic.septima_fts_config''::regconfig
+                ) desc,
+                ts_rank_cd(
+                    textsearchable_phonetic_col,
+                    to_tsquery(''simple'',$1)
+                )::double precision desc,
+                matrikelnummer,
+                visningstekst
+            LIMIT $3  ;', filters);
+        RETURN QUERY EXECUTE stmt
+        USING query_string, plain_query_string, rowlimit;
     END IF;
-    END
+END
 $function$;
 -- Test cases:
 /*
