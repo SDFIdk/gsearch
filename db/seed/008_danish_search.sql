d8975bc3cb626f93b69dcae69afe3cb5f74c96d6
Nobralyra <48592380+Nobralyra@users.noreply.github.com>
2023-02-08 16:25:07 +0100

    Revert "Add more Parsers as seen in DAWA to find out if it makes a difference in words like 2. Tværvej"
    This reverts commit 9f7593bc2878fa8e60a345fa5fb6821d52c64a32.

--- db/seed/008_danish_search.sql
+++ db/seed/008_danish_search.sql
@@ -27,6 +27,6 @@ ALTER TEXT SEARCH DICTIONARY unaccent (RULES = 'unaccent_gsearch');
 
 -- set the configuration to use the dictionaries in order: unaccent -> xsyn -> simply
 ALTER TEXT SEARCH CONFIGURATION basic.septima_fts_config
-    ALTER MAPPING FOR asciiword, asciihword, hword_asciipart, word, hword, hword_part, numword, numhword WITH unaccent,
+    ALTER MAPPING FOR asciiword, asciihword, hword_asciipart, word, hword, hword_part WITH unaccent,
     xsyn,
     simple;
