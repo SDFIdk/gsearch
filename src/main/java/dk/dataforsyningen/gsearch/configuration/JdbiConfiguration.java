d9b54ed54dd05a516dce45d3044432ec988b7872
Nobralyra <48592380+Nobralyra@users.noreply.github.com>
2023-03-10 10:43:26 +0100

    Add query timeout to be gently with the database.
    It needs to quit after Gravitee has send the correct 504 timeout http code

--- src/main/java/dk/dataforsyningen/gsearch/configuration/JdbiConfiguration.java
+++ src/main/java/dk/dataforsyningen/gsearch/configuration/JdbiConfiguration.java
@@ -2,9 +2,12 @@ package dk.dataforsyningen.gsearch.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dk.dataforsyningen.gsearch.ResourceTypes;
+import java.sql.SQLException;
+import java.util.List;
+import javax.sql.DataSource;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.core.mapper.RowMapper;
-import org.jdbi.v3.core.spi.JdbiPlugin;
+import org.jdbi.v3.core.statement.SqlStatements;
 import org.jdbi.v3.jackson2.Jackson2Plugin;
 import org.jdbi.v3.postgres.PostgresPlugin;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
@@ -17,10 +20,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
-
-import javax.sql.DataSource;
-import java.sql.SQLException;
-import java.util.List;
 
 @Configuration
 public class JdbiConfiguration {
@@ -51,6 +50,14 @@ public class JdbiConfiguration {
         determineTypes(jdbi);
         // TODO: Maybe this can be used to only register Row mapper once and not every time the getData gets called
         rowMappers.forEach(mapper -> jdbi.registerRowMapper(mapper));
+        // This cancels the sql statement so the database don't use unnecessary ressources on requests
+        // taking to long.
+        // Gravitee timeout is 10 seconds, and it sends the correct 504 timeout http code.
+        // In the code we set it to 11 seconds because it triggers the UnableToExecuteStatementException,
+        // that returns a 400 http code (bad request), but in this case it should have been a 504 timeout.
+        // So the 11 seconds is for always be later than Gravitee, but still cancels the ongoing statement
+        // from being executed longer
+        jdbi.getConfig(SqlStatements.class).setQueryTimeout(11);
         return jdbi;
     }
 
