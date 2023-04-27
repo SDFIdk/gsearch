315e3e7b6cadd949d2e3637c23074bf3b481b4b8
JesperKock <jekoc@sdfe.dk>
2023-04-27 12:27:26 +0200

    Improve formatting

--- db/seed/260_api_retskreds.sql
+++ db/seed/260_api_retskreds.sql
@@ -142,7 +142,12 @@ BEGIN
         tokens INTO plain_query_string;
     -- Execute and return the result
     stmt = format(E'SELECT
-                retskredsnummer::text, retkredsnavn::text, visningstekst::text, myndighedskode::text, geometri, bbox::geometry
+                retskredsnummer::text,
+                retkredsnavn::text,
+                visningstekst::text,
+                myndighedskode::text,
+                geometri,
+                bbox::geometry
             FROM
                 basic.retskreds
             WHERE (
@@ -151,7 +156,14 @@ BEGIN
                 OR textsearchable_plain_col @@ to_tsquery(''simple'', $2))
             AND %s
             ORDER BY
-                basic.combine_rank($2, $2, textsearchable_plain_col, textsearchable_unaccent_col, ''simple''::regconfig, ''basic.septima_fts_config''::regconfig) desc,
+                basic.combine_rank(
+                    $2,
+                    $2,
+                    textsearchable_plain_col,
+                    textsearchable_unaccent_col,
+                    ''simple''::regconfig,
+                    ''basic.septima_fts_config''::regconfig
+                ) desc,
                 ts_rank_cd(textsearchable_phonetic_col, to_tsquery(''simple'',$1))::double precision desc,
                 retkredsnavn
             LIMIT $3;', filters);
