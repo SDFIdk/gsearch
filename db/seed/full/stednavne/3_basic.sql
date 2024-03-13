393c8809f15e09f44ce4d95cbb53207f66e11b82
seskildsen <sines@sdfi.dk>
2024-03-13 12:21:36 +0100

    Revert "feat: try to see if the script runs faster with `with stednavne`"
    This reverts commit 1f9c3d52bcc33ec5b1dab69922276757de2d17b9.

--- db/seed/full/stednavne/3_basic.sql
+++ db/seed/full/stednavne/3_basic.sql
@@ -98,22 +98,7 @@ DROP TABLE IF EXISTS basic_initialloading.stednavn;
 --     subtype,
 --     kommunekode;
 
-WITH stednavne AS (
-    SELECT
-        objectid,
-        id_lokalid,
-        navnefoelgenummer,
-        visningstekst,
-        navnestatus,
-        skrivemaade,
-        type,
-        subtype,
-        kommunekode,
-        st_force2d (geometri_udtyndet) AS geometri
-    FROM
-        stednavne_udstilling.stednavne_udstilling
-),
-agg_stednavne_officiel AS (
+WITH agg_stednavne_officiel AS (
     SELECT
         objectid,
         skrivemaade
@@ -135,7 +120,7 @@ agg_stednavne_uofficiel AS (
 ),
 agg_stednavne AS (
     SELECT
-        s.objectid,
+        su.objectid,
         id_lokalid,
         navnefoelgenummer,
         visningstekst as visningstekst,
@@ -145,25 +130,24 @@ agg_stednavne AS (
         "type",
         subtype,
         kommunekode,
-        geometri
+        st_force2d (geometri_udtyndet) AS geometri
     FROM
-        stednavne s
+        stednavne_udstilling.stednavne_udstilling su
     LEFT JOIN agg_stednavne_officiel o ON
-        o.objectid = s.objectid
+        o.objectid = su.objectid
     LEFT JOIN agg_stednavne_uofficiel u ON
-        u.objectid = s.objectid
+        u.objectid = su.objectid
     GROUP BY
-        s.objectid,
+        su.objectid,
         id_lokalid,
         navnefoelgenummer,
         visningstekst,
-        navnestatus,
         o.skrivemaade,
         u.skrivemaader,
         "type",
         subtype,
         kommunekode,
-        geometri
+        geometri_udtyndet
 )
 SELECT
     id_lokalid AS id,
