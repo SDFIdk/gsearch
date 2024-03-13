6bc99153aee851dd0ba81ef14bb3860f929c4b15
seskildsen <sines@sdfi.dk>
2024-03-13 12:44:38 +0100

    feat: other uofficelle skrivemaader must not appear in result when
    searching for a place

--- db/seed/full/stednavne/4_postprocessing.sql
+++ db/seed/full/stednavne/4_postprocessing.sql
@@ -5,8 +5,7 @@ ALTER TABLE basic_initialloading.stednavn
     ADD COLUMN textsearchable_plain_col tsvector
     GENERATED ALWAYS AS (setweight(to_tsvector('simple', split_part(visningstekst, ' ', 1)), 'A') ||
                          setweight(to_tsvector('simple', split_part(visningstekst, ' ', 2)), 'B') ||
-                         setweight(to_tsvector('simple', functions.split_and_endsubstring ((visningstekst), 3)), 'C') ||
-                         functions.stednavne_uofficielle_tsvector (skrivemaade_uofficiel))
+                         setweight(to_tsvector('simple', functions.split_and_endsubstring ((visningstekst), 3)), 'C'))
     STORED;
 
 ALTER TABLE basic_initialloading.stednavn
@@ -16,8 +15,7 @@ ALTER TABLE basic_initialloading.stednavn
     ADD COLUMN textsearchable_unaccent_col tsvector
     GENERATED ALWAYS AS (setweight(to_tsvector('functions.gsearch_fts_config', split_part(visningstekst, ' ', 1)), 'A') ||
                          setweight(to_tsvector('functions.gsearch_fts_config', split_part(visningstekst, ' ', 2)), 'B') ||
-                         setweight(to_tsvector('functions.gsearch_fts_config', functions.split_and_endsubstring (visningstekst, 3)), 'C') ||
-                         functions.stednavne_uofficielle_tsvector (skrivemaade_uofficiel))
+                         setweight(to_tsvector('functions.gsearch_fts_config', functions.split_and_endsubstring (visningstekst, 3)), 'C'))
     STORED;
 
 ALTER TABLE basic_initialloading.stednavn
@@ -27,7 +25,6 @@ ALTER TABLE basic_initialloading.stednavn
     ADD COLUMN textsearchable_phonetic_col tsvector
     GENERATED ALWAYS AS (setweight(to_tsvector('simple', functions.fnfonetik (split_part(visningstekst_nohyphen, ' ', 1), 2)), 'A') ||
                          setweight(to_tsvector('simple', functions.fnfonetik (split_part(visningstekst_nohyphen, ' ', 2), 2)), 'B') ||
-                         setweight(to_tsvector('simple', functions.split_and_endsubstring_fonetik (visningstekst_nohyphen, 3)), 'C') ||
-                         functions.stednavne_uofficielle_tsvector_phonetic (skrivemaade_uofficiel_nohyphen))
+                         setweight(to_tsvector('simple', functions.split_and_endsubstring_fonetik (visningstekst_nohyphen, 3)), 'C'))
     STORED;
     
