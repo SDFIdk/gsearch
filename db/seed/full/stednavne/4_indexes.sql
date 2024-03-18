47c3779459ffadf926239dca8edbadfbb760e364
seskildsen <sines@sdfi.dk>
2024-03-18 12:41:46 +0100

    feat: add index on visningstekst

--- db/seed/full/stednavne/4_indexes.sql
+++ db/seed/full/stednavne/4_indexes.sql
@@ -1,5 +1,7 @@
 CREATE INDEX ON basic_initialloading.stednavn (lower(visningstekst));
 
+CREATE INDEX ON basic_initialloading.stednavn (visningstekst);
+
 CREATE INDEX ON basic_initialloading.stednavn (kommunekode);
 
 CREATE INDEX ON basic_initialloading.stednavn USING gist (geometri);
