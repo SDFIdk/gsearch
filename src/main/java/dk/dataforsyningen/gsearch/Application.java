64751e7506d09b0f365b92447c0166ef01dfa30a
seskildsen <sines@sdfi.dk>
2025-05-22 17:29:31 +0200

    refactor: gsearch has never used commandLineRunner

--- src/main/java/dk/dataforsyningen/gsearch/Application.java
+++ src/main/java/dk/dataforsyningen/gsearch/Application.java
@@ -1,22 +1,12 @@
 package dk.dataforsyningen.gsearch;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-
-    // TODO: Can maybe be deleted
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-        };
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 }
